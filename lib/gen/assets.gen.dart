02f62cd8a9d1176095768351b468ad0726d99b71
codenameakshay <akshaymaurya3006@gmail.com>
2024-02-06 22:48:17 +0530

    [add]: card svgs

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -13,8 +13,7 @@ class $AssetsImagesGen {
   const $AssetsImagesGen();
 
   /// File path: assets/images/app_icon.png
-  AssetGenImage get appIcon =>
-      const AssetGenImage('assets/images/app_icon.png');
+  AssetGenImage get appIcon => const AssetGenImage('assets/images/app_icon.png');
 
   /// List of all assets
   List<AssetGenImage> get values => [appIcon];
@@ -33,11 +32,340 @@ class $AssetsLottiesGen {
   List<String> get values => [confetti, snow];
 }
 
+class $AssetsSvgsGen {
+  const $AssetsSvgsGen();
+
+  $AssetsSvgsBacksGen get backs => const $AssetsSvgsBacksGen();
+  $AssetsSvgsFrontsGen get fronts => const $AssetsSvgsFrontsGen();
+  $AssetsSvgsOtherGen get other => const $AssetsSvgsOtherGen();
+}
+
+class $AssetsSvgsBacksGen {
+  const $AssetsSvgsBacksGen();
+
+  /// File path: assets/svgs/backs/abstract.svg
+  String get abstract => 'assets/svgs/backs/abstract.svg';
+
+  /// File path: assets/svgs/backs/abstract_clouds.svg
+  String get abstractClouds => 'assets/svgs/backs/abstract_clouds.svg';
+
+  /// File path: assets/svgs/backs/abstract_scene.svg
+  String get abstractScene => 'assets/svgs/backs/abstract_scene.svg';
+
+  /// File path: assets/svgs/backs/astronaut.svg
+  String get astronaut => 'assets/svgs/backs/astronaut.svg';
+
+  /// File path: assets/svgs/backs/blue.svg
+  String get blue => 'assets/svgs/backs/blue.svg';
+
+  /// File path: assets/svgs/backs/blue2.svg
+  String get blue2 => 'assets/svgs/backs/blue2.svg';
+
+  /// File path: assets/svgs/backs/cars.svg
+  String get cars => 'assets/svgs/backs/cars.svg';
+
+  /// File path: assets/svgs/backs/castle.svg
+  String get castle => 'assets/svgs/backs/castle.svg';
+
+  /// File path: assets/svgs/backs/fish.svg
+  String get fish => 'assets/svgs/backs/fish.svg';
+
+  /// File path: assets/svgs/backs/frog.svg
+  String get frog => 'assets/svgs/backs/frog.svg';
+
+  /// File path: assets/svgs/backs/red.svg
+  String get red => 'assets/svgs/backs/red.svg';
+
+  /// File path: assets/svgs/backs/red2.svg
+  String get red2 => 'assets/svgs/backs/red2.svg';
+
+  /// List of all assets
+  List<String> get values =>
+      [abstract, abstractClouds, abstractScene, astronaut, blue, blue2, cars, castle, fish, frog, red, red2];
+}
+
+class $AssetsSvgsFrontsGen {
+  const $AssetsSvgsFrontsGen();
+
+  /// File path: assets/svgs/fronts/clubs_10.svg
+  String get clubs10 => 'assets/svgs/fronts/clubs_10.svg';
+
+  /// File path: assets/svgs/fronts/clubs_2.svg
+  String get clubs2 => 'assets/svgs/fronts/clubs_2.svg';
+
+  /// File path: assets/svgs/fronts/clubs_3.svg
+  String get clubs3 => 'assets/svgs/fronts/clubs_3.svg';
+
+  /// File path: assets/svgs/fronts/clubs_4.svg
+  String get clubs4 => 'assets/svgs/fronts/clubs_4.svg';
+
+  /// File path: assets/svgs/fronts/clubs_5.svg
+  String get clubs5 => 'assets/svgs/fronts/clubs_5.svg';
+
+  /// File path: assets/svgs/fronts/clubs_6.svg
+  String get clubs6 => 'assets/svgs/fronts/clubs_6.svg';
+
+  /// File path: assets/svgs/fronts/clubs_7.svg
+  String get clubs7 => 'assets/svgs/fronts/clubs_7.svg';
+
+  /// File path: assets/svgs/fronts/clubs_8.svg
+  String get clubs8 => 'assets/svgs/fronts/clubs_8.svg';
+
+  /// File path: assets/svgs/fronts/clubs_9.svg
+  String get clubs9 => 'assets/svgs/fronts/clubs_9.svg';
+
+  /// File path: assets/svgs/fronts/clubs_ace.svg
+  String get clubsAce => 'assets/svgs/fronts/clubs_ace.svg';
+
+  /// File path: assets/svgs/fronts/clubs_jack.svg
+  String get clubsJack => 'assets/svgs/fronts/clubs_jack.svg';
+
+  /// File path: assets/svgs/fronts/clubs_king.svg
+  String get clubsKing => 'assets/svgs/fronts/clubs_king.svg';
+
+  /// File path: assets/svgs/fronts/clubs_queen.svg
+  String get clubsQueen => 'assets/svgs/fronts/clubs_queen.svg';
+
+  /// File path: assets/svgs/fronts/diamonds_10.svg
+  String get diamonds10 => 'assets/svgs/fronts/diamonds_10.svg';
+
+  /// File path: assets/svgs/fronts/diamonds_2.svg
+  String get diamonds2 => 'assets/svgs/fronts/diamonds_2.svg';
+
+  /// File path: assets/svgs/fronts/diamonds_3.svg
+  String get diamonds3 => 'assets/svgs/fronts/diamonds_3.svg';
+
+  /// File path: assets/svgs/fronts/diamonds_4.svg
+  String get diamonds4 => 'assets/svgs/fronts/diamonds_4.svg';
+
+  /// File path: assets/svgs/fronts/diamonds_5.svg
+  String get diamonds5 => 'assets/svgs/fronts/diamonds_5.svg';
+
+  /// File path: assets/svgs/fronts/diamonds_6.svg
+  String get diamonds6 => 'assets/svgs/fronts/diamonds_6.svg';
+
+  /// File path: assets/svgs/fronts/diamonds_7.svg
+  String get diamonds7 => 'assets/svgs/fronts/diamonds_7.svg';
+
+  /// File path: assets/svgs/fronts/diamonds_8.svg
+  String get diamonds8 => 'assets/svgs/fronts/diamonds_8.svg';
+
+  /// File path: assets/svgs/fronts/diamonds_9.svg
+  String get diamonds9 => 'assets/svgs/fronts/diamonds_9.svg';
+
+  /// File path: assets/svgs/fronts/diamonds_ace.svg
+  String get diamondsAce => 'assets/svgs/fronts/diamonds_ace.svg';
+
+  /// File path: assets/svgs/fronts/diamonds_jack.svg
+  String get diamondsJack => 'assets/svgs/fronts/diamonds_jack.svg';
+
+  /// File path: assets/svgs/fronts/diamonds_king.svg
+  String get diamondsKing => 'assets/svgs/fronts/diamonds_king.svg';
+
+  /// File path: assets/svgs/fronts/diamonds_queen.svg
+  String get diamondsQueen => 'assets/svgs/fronts/diamonds_queen.svg';
+
+  /// File path: assets/svgs/fronts/hearts_10.svg
+  String get hearts10 => 'assets/svgs/fronts/hearts_10.svg';
+
+  /// File path: assets/svgs/fronts/hearts_2.svg
+  String get hearts2 => 'assets/svgs/fronts/hearts_2.svg';
+
+  /// File path: assets/svgs/fronts/hearts_3.svg
+  String get hearts3 => 'assets/svgs/fronts/hearts_3.svg';
+
+  /// File path: assets/svgs/fronts/hearts_4.svg
+  String get hearts4 => 'assets/svgs/fronts/hearts_4.svg';
+
+  /// File path: assets/svgs/fronts/hearts_5.svg
+  String get hearts5 => 'assets/svgs/fronts/hearts_5.svg';
+
+  /// File path: assets/svgs/fronts/hearts_6.svg
+  String get hearts6 => 'assets/svgs/fronts/hearts_6.svg';
+
+  /// File path: assets/svgs/fronts/hearts_7.svg
+  String get hearts7 => 'assets/svgs/fronts/hearts_7.svg';
+
+  /// File path: assets/svgs/fronts/hearts_8.svg
+  String get hearts8 => 'assets/svgs/fronts/hearts_8.svg';
+
+  /// File path: assets/svgs/fronts/hearts_9.svg
+  String get hearts9 => 'assets/svgs/fronts/hearts_9.svg';
+
+  /// File path: assets/svgs/fronts/hearts_ace.svg
+  String get heartsAce => 'assets/svgs/fronts/hearts_ace.svg';
+
+  /// File path: assets/svgs/fronts/hearts_jack.svg
+  String get heartsJack => 'assets/svgs/fronts/hearts_jack.svg';
+
+  /// File path: assets/svgs/fronts/hearts_king.svg
+  String get heartsKing => 'assets/svgs/fronts/hearts_king.svg';
+
+  /// File path: assets/svgs/fronts/hearts_queen.svg
+  String get heartsQueen => 'assets/svgs/fronts/hearts_queen.svg';
+
+  /// File path: assets/svgs/fronts/joker_black.svg
+  String get jokerBlack => 'assets/svgs/fronts/joker_black.svg';
+
+  /// File path: assets/svgs/fronts/joker_red.svg
+  String get jokerRed => 'assets/svgs/fronts/joker_red.svg';
+
+  /// File path: assets/svgs/fronts/spades_10.svg
+  String get spades10 => 'assets/svgs/fronts/spades_10.svg';
+
+  /// File path: assets/svgs/fronts/spades_2.svg
+  String get spades2 => 'assets/svgs/fronts/spades_2.svg';
+
+  /// File path: assets/svgs/fronts/spades_3.svg
+  String get spades3 => 'assets/svgs/fronts/spades_3.svg';
+
+  /// File path: assets/svgs/fronts/spades_4.svg
+  String get spades4 => 'assets/svgs/fronts/spades_4.svg';
+
+  /// File path: assets/svgs/fronts/spades_5.svg
+  String get spades5 => 'assets/svgs/fronts/spades_5.svg';
+
+  /// File path: assets/svgs/fronts/spades_6.svg
+  String get spades6 => 'assets/svgs/fronts/spades_6.svg';
+
+  /// File path: assets/svgs/fronts/spades_7.svg
+  String get spades7 => 'assets/svgs/fronts/spades_7.svg';
+
+  /// File path: assets/svgs/fronts/spades_8.svg
+  String get spades8 => 'assets/svgs/fronts/spades_8.svg';
+
+  /// File path: assets/svgs/fronts/spades_9.svg
+  String get spades9 => 'assets/svgs/fronts/spades_9.svg';
+
+  /// File path: assets/svgs/fronts/spades_ace.svg
+  String get spadesAce => 'assets/svgs/fronts/spades_ace.svg';
+
+  /// File path: assets/svgs/fronts/spades_jack.svg
+  String get spadesJack => 'assets/svgs/fronts/spades_jack.svg';
+
+  /// File path: assets/svgs/fronts/spades_king.svg
+  String get spadesKing => 'assets/svgs/fronts/spades_king.svg';
+
+  /// File path: assets/svgs/fronts/spades_queen.svg
+  String get spadesQueen => 'assets/svgs/fronts/spades_queen.svg';
+
+  /// List of all assets
+  List<String> get values => [
+        clubs10,
+        clubs2,
+        clubs3,
+        clubs4,
+        clubs5,
+        clubs6,
+        clubs7,
+        clubs8,
+        clubs9,
+        clubsAce,
+        clubsJack,
+        clubsKing,
+        clubsQueen,
+        diamonds10,
+        diamonds2,
+        diamonds3,
+        diamonds4,
+        diamonds5,
+        diamonds6,
+        diamonds7,
+        diamonds8,
+        diamonds9,
+        diamondsAce,
+        diamondsJack,
+        diamondsKing,
+        diamondsQueen,
+        hearts10,
+        hearts2,
+        hearts3,
+        hearts4,
+        hearts5,
+        hearts6,
+        hearts7,
+        hearts8,
+        hearts9,
+        heartsAce,
+        heartsJack,
+        heartsKing,
+        heartsQueen,
+        jokerBlack,
+        jokerRed,
+        spades10,
+        spades2,
+        spades3,
+        spades4,
+        spades5,
+        spades6,
+        spades7,
+        spades8,
+        spades9,
+        spadesAce,
+        spadesJack,
+        spadesKing,
+        spadesQueen
+      ];
+}
+
+class $AssetsSvgsOtherGen {
+  const $AssetsSvgsOtherGen();
+
+  /// File path: assets/svgs/other/blank_card.svg
+  String get blankCard => 'assets/svgs/other/blank_card.svg';
+
+  /// File path: assets/svgs/other/clubs_8_alt.svg
+  String get clubs8Alt => 'assets/svgs/other/clubs_8_alt.svg';
+
+  /// File path: assets/svgs/other/clubs_ace_large_pip.svg
+  String get clubsAceLargePip => 'assets/svgs/other/clubs_ace_large_pip.svg';
+
+  /// File path: assets/svgs/other/diamonds_8_alt.svg
+  String get diamonds8Alt => 'assets/svgs/other/diamonds_8_alt.svg';
+
+  /// File path: assets/svgs/other/diamonds_ace_large_pip.svg
+  String get diamondsAceLargePip => 'assets/svgs/other/diamonds_ace_large_pip.svg';
+
+  /// File path: assets/svgs/other/hearts_8_alt.svg
+  String get hearts8Alt => 'assets/svgs/other/hearts_8_alt.svg';
+
+  /// File path: assets/svgs/other/hearts_ace_large_pip.svg
+  String get heartsAceLargePip => 'assets/svgs/other/hearts_ace_large_pip.svg';
+
+  /// File path: assets/svgs/other/spades_8_alt.svg
+  String get spades8Alt => 'assets/svgs/other/spades_8_alt.svg';
+
+  /// File path: assets/svgs/other/spades_ace_large_pip.svg
+  String get spadesAceLargePip => 'assets/svgs/other/spades_ace_large_pip.svg';
+
+  /// File path: assets/svgs/other/spades_ace_no_phrase.svg
+  String get spadesAceNoPhrase => 'assets/svgs/other/spades_ace_no_phrase.svg';
+
+  /// File path: assets/svgs/other/spades_ace_simple.svg
+  String get spadesAceSimple => 'assets/svgs/other/spades_ace_simple.svg';
+
+  /// List of all assets
+  List<String> get values => [
+        blankCard,
+        clubs8Alt,
+        clubsAceLargePip,
+        diamonds8Alt,
+        diamondsAceLargePip,
+        hearts8Alt,
+        heartsAceLargePip,
+        spades8Alt,
+        spadesAceLargePip,
+        spadesAceNoPhrase,
+        spadesAceSimple
+      ];
+}
+
 class Assets {
   Assets._();
 
   static const $AssetsImagesGen images = $AssetsImagesGen();
   static const $AssetsLottiesGen lotties = $AssetsLottiesGen();
+  static const $AssetsSvgsGen svgs = $AssetsSvgsGen();
 }
 
 class AssetGenImage {
