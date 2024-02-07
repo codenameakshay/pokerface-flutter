6ebaaeabe9da6097eb4a96ff039f735e8a5cd9d5
codenameakshay <akshaymaurya3006@gmail.com>
2024-02-07 13:47:18 +0530

    [add]: cards png images

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -15,6 +15,10 @@ class $AssetsImagesGen {
   /// File path: assets/images/app_icon.png
   AssetGenImage get appIcon => const AssetGenImage('assets/images/app_icon.png');
 
+  $AssetsImagesBacksGen get backs => const $AssetsImagesBacksGen();
+  $AssetsImagesFrontsGen get fronts => const $AssetsImagesFrontsGen();
+  $AssetsImagesOtherGen get other => const $AssetsImagesOtherGen();
+
   /// List of all assets
   List<AssetGenImage> get values => [appIcon];
 }
@@ -48,6 +52,326 @@ class $AssetsSvgsGen {
   $AssetsSvgsBacksGen get backs => const $AssetsSvgsBacksGen();
   $AssetsSvgsFrontsGen get fronts => const $AssetsSvgsFrontsGen();
   $AssetsSvgsOtherGen get other => const $AssetsSvgsOtherGen();
+}
+
+class $AssetsImagesBacksGen {
+  const $AssetsImagesBacksGen();
+
+  /// File path: assets/images/backs/abstract.png
+  AssetGenImage get abstract => const AssetGenImage('assets/images/backs/abstract.png');
+
+  /// File path: assets/images/backs/abstract_clouds.png
+  AssetGenImage get abstractClouds => const AssetGenImage('assets/images/backs/abstract_clouds.png');
+
+  /// File path: assets/images/backs/abstract_scene.png
+  AssetGenImage get abstractScene => const AssetGenImage('assets/images/backs/abstract_scene.png');
+
+  /// File path: assets/images/backs/astronaut.png
+  AssetGenImage get astronaut => const AssetGenImage('assets/images/backs/astronaut.png');
+
+  /// File path: assets/images/backs/blue.png
+  AssetGenImage get blue => const AssetGenImage('assets/images/backs/blue.png');
+
+  /// File path: assets/images/backs/blue2.png
+  AssetGenImage get blue2 => const AssetGenImage('assets/images/backs/blue2.png');
+
+  /// File path: assets/images/backs/cars.png
+  AssetGenImage get cars => const AssetGenImage('assets/images/backs/cars.png');
+
+  /// File path: assets/images/backs/castle.png
+  AssetGenImage get castle => const AssetGenImage('assets/images/backs/castle.png');
+
+  /// File path: assets/images/backs/fish.png
+  AssetGenImage get fish => const AssetGenImage('assets/images/backs/fish.png');
+
+  /// File path: assets/images/backs/frog.png
+  AssetGenImage get frog => const AssetGenImage('assets/images/backs/frog.png');
+
+  /// File path: assets/images/backs/red.png
+  AssetGenImage get red => const AssetGenImage('assets/images/backs/red.png');
+
+  /// File path: assets/images/backs/red2.png
+  AssetGenImage get red2 => const AssetGenImage('assets/images/backs/red2.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values =>
+      [abstract, abstractClouds, abstractScene, astronaut, blue, blue2, cars, castle, fish, frog, red, red2];
+}
+
+class $AssetsImagesFrontsGen {
+  const $AssetsImagesFrontsGen();
+
+  /// File path: assets/images/fronts/clubs_10.png
+  AssetGenImage get clubs10 => const AssetGenImage('assets/images/fronts/clubs_10.png');
+
+  /// File path: assets/images/fronts/clubs_2.png
+  AssetGenImage get clubs2 => const AssetGenImage('assets/images/fronts/clubs_2.png');
+
+  /// File path: assets/images/fronts/clubs_3.png
+  AssetGenImage get clubs3 => const AssetGenImage('assets/images/fronts/clubs_3.png');
+
+  /// File path: assets/images/fronts/clubs_4.png
+  AssetGenImage get clubs4 => const AssetGenImage('assets/images/fronts/clubs_4.png');
+
+  /// File path: assets/images/fronts/clubs_5.png
+  AssetGenImage get clubs5 => const AssetGenImage('assets/images/fronts/clubs_5.png');
+
+  /// File path: assets/images/fronts/clubs_6.png
+  AssetGenImage get clubs6 => const AssetGenImage('assets/images/fronts/clubs_6.png');
+
+  /// File path: assets/images/fronts/clubs_7.png
+  AssetGenImage get clubs7 => const AssetGenImage('assets/images/fronts/clubs_7.png');
+
+  /// File path: assets/images/fronts/clubs_8.png
+  AssetGenImage get clubs8 => const AssetGenImage('assets/images/fronts/clubs_8.png');
+
+  /// File path: assets/images/fronts/clubs_9.png
+  AssetGenImage get clubs9 => const AssetGenImage('assets/images/fronts/clubs_9.png');
+
+  /// File path: assets/images/fronts/clubs_ace.png
+  AssetGenImage get clubsAce => const AssetGenImage('assets/images/fronts/clubs_ace.png');
+
+  /// File path: assets/images/fronts/clubs_jack.png
+  AssetGenImage get clubsJack => const AssetGenImage('assets/images/fronts/clubs_jack.png');
+
+  /// File path: assets/images/fronts/clubs_king.png
+  AssetGenImage get clubsKing => const AssetGenImage('assets/images/fronts/clubs_king.png');
+
+  /// File path: assets/images/fronts/clubs_queen.png
+  AssetGenImage get clubsQueen => const AssetGenImage('assets/images/fronts/clubs_queen.png');
+
+  /// File path: assets/images/fronts/diamonds_10.png
+  AssetGenImage get diamonds10 => const AssetGenImage('assets/images/fronts/diamonds_10.png');
+
+  /// File path: assets/images/fronts/diamonds_2.png
+  AssetGenImage get diamonds2 => const AssetGenImage('assets/images/fronts/diamonds_2.png');
+
+  /// File path: assets/images/fronts/diamonds_3.png
+  AssetGenImage get diamonds3 => const AssetGenImage('assets/images/fronts/diamonds_3.png');
+
+  /// File path: assets/images/fronts/diamonds_4.png
+  AssetGenImage get diamonds4 => const AssetGenImage('assets/images/fronts/diamonds_4.png');
+
+  /// File path: assets/images/fronts/diamonds_5.png
+  AssetGenImage get diamonds5 => const AssetGenImage('assets/images/fronts/diamonds_5.png');
+
+  /// File path: assets/images/fronts/diamonds_6.png
+  AssetGenImage get diamonds6 => const AssetGenImage('assets/images/fronts/diamonds_6.png');
+
+  /// File path: assets/images/fronts/diamonds_7.png
+  AssetGenImage get diamonds7 => const AssetGenImage('assets/images/fronts/diamonds_7.png');
+
+  /// File path: assets/images/fronts/diamonds_8.png
+  AssetGenImage get diamonds8 => const AssetGenImage('assets/images/fronts/diamonds_8.png');
+
+  /// File path: assets/images/fronts/diamonds_9.png
+  AssetGenImage get diamonds9 => const AssetGenImage('assets/images/fronts/diamonds_9.png');
+
+  /// File path: assets/images/fronts/diamonds_ace.png
+  AssetGenImage get diamondsAce => const AssetGenImage('assets/images/fronts/diamonds_ace.png');
+
+  /// File path: assets/images/fronts/diamonds_jack.png
+  AssetGenImage get diamondsJack => const AssetGenImage('assets/images/fronts/diamonds_jack.png');
+
+  /// File path: assets/images/fronts/diamonds_king.png
+  AssetGenImage get diamondsKing => const AssetGenImage('assets/images/fronts/diamonds_king.png');
+
+  /// File path: assets/images/fronts/diamonds_queen.png
+  AssetGenImage get diamondsQueen => const AssetGenImage('assets/images/fronts/diamonds_queen.png');
+
+  /// File path: assets/images/fronts/hearts_10.png
+  AssetGenImage get hearts10 => const AssetGenImage('assets/images/fronts/hearts_10.png');
+
+  /// File path: assets/images/fronts/hearts_2.png
+  AssetGenImage get hearts2 => const AssetGenImage('assets/images/fronts/hearts_2.png');
+
+  /// File path: assets/images/fronts/hearts_3.png
+  AssetGenImage get hearts3 => const AssetGenImage('assets/images/fronts/hearts_3.png');
+
+  /// File path: assets/images/fronts/hearts_4.png
+  AssetGenImage get hearts4 => const AssetGenImage('assets/images/fronts/hearts_4.png');
+
+  /// File path: assets/images/fronts/hearts_5.png
+  AssetGenImage get hearts5 => const AssetGenImage('assets/images/fronts/hearts_5.png');
+
+  /// File path: assets/images/fronts/hearts_6.png
+  AssetGenImage get hearts6 => const AssetGenImage('assets/images/fronts/hearts_6.png');
+
+  /// File path: assets/images/fronts/hearts_7.png
+  AssetGenImage get hearts7 => const AssetGenImage('assets/images/fronts/hearts_7.png');
+
+  /// File path: assets/images/fronts/hearts_8.png
+  AssetGenImage get hearts8 => const AssetGenImage('assets/images/fronts/hearts_8.png');
+
+  /// File path: assets/images/fronts/hearts_9.png
+  AssetGenImage get hearts9 => const AssetGenImage('assets/images/fronts/hearts_9.png');
+
+  /// File path: assets/images/fronts/hearts_ace.png
+  AssetGenImage get heartsAce => const AssetGenImage('assets/images/fronts/hearts_ace.png');
+
+  /// File path: assets/images/fronts/hearts_jack.png
+  AssetGenImage get heartsJack => const AssetGenImage('assets/images/fronts/hearts_jack.png');
+
+  /// File path: assets/images/fronts/hearts_king.png
+  AssetGenImage get heartsKing => const AssetGenImage('assets/images/fronts/hearts_king.png');
+
+  /// File path: assets/images/fronts/hearts_queen.png
+  AssetGenImage get heartsQueen => const AssetGenImage('assets/images/fronts/hearts_queen.png');
+
+  /// File path: assets/images/fronts/joker_black.png
+  AssetGenImage get jokerBlack => const AssetGenImage('assets/images/fronts/joker_black.png');
+
+  /// File path: assets/images/fronts/joker_red.png
+  AssetGenImage get jokerRed => const AssetGenImage('assets/images/fronts/joker_red.png');
+
+  /// File path: assets/images/fronts/spades_10.png
+  AssetGenImage get spades10 => const AssetGenImage('assets/images/fronts/spades_10.png');
+
+  /// File path: assets/images/fronts/spades_2.png
+  AssetGenImage get spades2 => const AssetGenImage('assets/images/fronts/spades_2.png');
+
+  /// File path: assets/images/fronts/spades_3.png
+  AssetGenImage get spades3 => const AssetGenImage('assets/images/fronts/spades_3.png');
+
+  /// File path: assets/images/fronts/spades_4.png
+  AssetGenImage get spades4 => const AssetGenImage('assets/images/fronts/spades_4.png');
+
+  /// File path: assets/images/fronts/spades_5.png
+  AssetGenImage get spades5 => const AssetGenImage('assets/images/fronts/spades_5.png');
+
+  /// File path: assets/images/fronts/spades_6.png
+  AssetGenImage get spades6 => const AssetGenImage('assets/images/fronts/spades_6.png');
+
+  /// File path: assets/images/fronts/spades_7.png
+  AssetGenImage get spades7 => const AssetGenImage('assets/images/fronts/spades_7.png');
+
+  /// File path: assets/images/fronts/spades_8.png
+  AssetGenImage get spades8 => const AssetGenImage('assets/images/fronts/spades_8.png');
+
+  /// File path: assets/images/fronts/spades_9.png
+  AssetGenImage get spades9 => const AssetGenImage('assets/images/fronts/spades_9.png');
+
+  /// File path: assets/images/fronts/spades_ace.png
+  AssetGenImage get spadesAce => const AssetGenImage('assets/images/fronts/spades_ace.png');
+
+  /// File path: assets/images/fronts/spades_jack.png
+  AssetGenImage get spadesJack => const AssetGenImage('assets/images/fronts/spades_jack.png');
+
+  /// File path: assets/images/fronts/spades_king.png
+  AssetGenImage get spadesKing => const AssetGenImage('assets/images/fronts/spades_king.png');
+
+  /// File path: assets/images/fronts/spades_queen.png
+  AssetGenImage get spadesQueen => const AssetGenImage('assets/images/fronts/spades_queen.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        clubs10,
+        clubs2,
+        clubs3,
+        clubs4,
+        clubs5,
+        clubs6,
+        clubs7,
+        clubs8,
+        clubs9,
+        clubsAce,
+        clubsJack,
+        clubsKing,
+        clubsQueen,
+        diamonds10,
+        diamonds2,
+        diamonds3,
+        diamonds4,
+        diamonds5,
+        diamonds6,
+        diamonds7,
+        diamonds8,
+        diamonds9,
+        diamondsAce,
+        diamondsJack,
+        diamondsKing,
+        diamondsQueen,
+        hearts10,
+        hearts2,
+        hearts3,
+        hearts4,
+        hearts5,
+        hearts6,
+        hearts7,
+        hearts8,
+        hearts9,
+        heartsAce,
+        heartsJack,
+        heartsKing,
+        heartsQueen,
+        jokerBlack,
+        jokerRed,
+        spades10,
+        spades2,
+        spades3,
+        spades4,
+        spades5,
+        spades6,
+        spades7,
+        spades8,
+        spades9,
+        spadesAce,
+        spadesJack,
+        spadesKing,
+        spadesQueen
+      ];
+}
+
+class $AssetsImagesOtherGen {
+  const $AssetsImagesOtherGen();
+
+  /// File path: assets/images/other/blank_card.png
+  AssetGenImage get blankCard => const AssetGenImage('assets/images/other/blank_card.png');
+
+  /// File path: assets/images/other/clubs_8_alt.png
+  AssetGenImage get clubs8Alt => const AssetGenImage('assets/images/other/clubs_8_alt.png');
+
+  /// File path: assets/images/other/clubs_ace_large_pip.png
+  AssetGenImage get clubsAceLargePip => const AssetGenImage('assets/images/other/clubs_ace_large_pip.png');
+
+  /// File path: assets/images/other/diamonds_8_alt.png
+  AssetGenImage get diamonds8Alt => const AssetGenImage('assets/images/other/diamonds_8_alt.png');
+
+  /// File path: assets/images/other/diamonds_ace_large_pip.png
+  AssetGenImage get diamondsAceLargePip => const AssetGenImage('assets/images/other/diamonds_ace_large_pip.png');
+
+  /// File path: assets/images/other/hearts_8_alt.png
+  AssetGenImage get hearts8Alt => const AssetGenImage('assets/images/other/hearts_8_alt.png');
+
+  /// File path: assets/images/other/hearts_ace_large_pip.png
+  AssetGenImage get heartsAceLargePip => const AssetGenImage('assets/images/other/hearts_ace_large_pip.png');
+
+  /// File path: assets/images/other/spades_8_alt.png
+  AssetGenImage get spades8Alt => const AssetGenImage('assets/images/other/spades_8_alt.png');
+
+  /// File path: assets/images/other/spades_ace_large_pip.png
+  AssetGenImage get spadesAceLargePip => const AssetGenImage('assets/images/other/spades_ace_large_pip.png');
+
+  /// File path: assets/images/other/spades_ace_no_phrase.png
+  AssetGenImage get spadesAceNoPhrase => const AssetGenImage('assets/images/other/spades_ace_no_phrase.png');
+
+  /// File path: assets/images/other/spades_ace_simple.png
+  AssetGenImage get spadesAceSimple => const AssetGenImage('assets/images/other/spades_ace_simple.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        blankCard,
+        clubs8Alt,
+        clubsAceLargePip,
+        diamonds8Alt,
+        diamondsAceLargePip,
+        hearts8Alt,
+        heartsAceLargePip,
+        spades8Alt,
+        spadesAceLargePip,
+        spadesAceNoPhrase,
+        spadesAceSimple
+      ];
 }
 
 class $AssetsSvgsBacksGen {
