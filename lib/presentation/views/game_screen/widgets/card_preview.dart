d0de4125d710415b08305381556fddcf67292694
codenameakshay <akshaymaurya3006@gmail.com>
2024-02-13 16:49:22 +0530

    [fix]: card preview to highlight user cards

--- lib/presentation/views/game_screen/widgets/card_preview.dart
+++ lib/presentation/views/game_screen/widgets/card_preview.dart
@@ -3,19 +3,33 @@ part of '../view.dart';
 class CardPreview extends ConsumerWidget {
   final double width;
   final Card card;
+  final List<Card> userCards;
+  final ThemeState theme;
 
   const CardPreview({
     super.key,
     required this.width,
     required this.card,
+    required this.userCards,
+    required this.theme,
   });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return SizedBox(
+    return Container(
       width: width,
       height: (333 / 234) * width,
-      child: CardsPNG.drawCard(card.image.png),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4),
+        border: userCards.contains(card)
+            ? Border.all(
+                color: theme.colors.primary,
+                width: 2,
+                strokeAlign: BorderSide.strokeAlignInside,
+              )
+            : null,
+      ),
+      child: CardsPNG.drawCard(card.image.png, fit: BoxFit.cover),
     );
   }
 }
